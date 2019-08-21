7c97390d07d787b72a1a26d3f4f977b7d35a2909
Martin Čížek <martin.cizek@orchitech.cz>
2019-08-21 10:42:37 +0200

    Update plugin descriptior.

--- init.rb
+++ init.rb
@@ -1,11 +1,10 @@
 Redmine::Plugin.register :redmine_convert_textile_to_markdown do
   name 'Redmine Textile -> Markdown migration Plugin'
-  author 'Jens Krämer, Planio GmbH'
-  author_url 'https://plan.io/'
-  description ''
-  version '0.1.0'
+  author 'Martin Cizek, Orchitech Solutions'
+  author_url 'https://orchi.tech/'
+  description 'Rake task providing Textile to GFM conversion based on former Ecodev and plan.io work. Uses pandoc and heavy pre/post processing.'
+  version '0.9.0'
   hidden true if respond_to? :hidden
 
   requires_redmine version_or_higher: '3.3.0'
 end
-
